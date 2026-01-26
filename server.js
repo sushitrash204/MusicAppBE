d6942b483ca2d2bd33607068769036db77e3f338
NguyenThanhNghia <147211357+Nghiakuu@users.noreply.github.com>
2026-01-26 12:23:12 +0700

    conect cloundinary

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 const { connectToDb, getDb } = require('./src/config/database');
+const uploadCloud = require('./src/config/cloudinary');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const cloudinary = require('cloudinary').v2;
 
 connectToDb((err) => {
     if (!err) {
         app.listen(PORT, () => {
             console.log(`App listening on port ${PORT}`);
+            cloudinary.api.ping()
+                .then(result => console.log('Cloudinary connection status:', result.status))
+                .catch(error => console.error('Cloudinary connection failed:', error.message));
         });
     } else {
         console.error('Failed to connect to database', err);
